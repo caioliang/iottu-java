504506f58749d39827c1ecacbae4cf4d35ac9e5c
Caio Liang <caioliang2@gmail.com>
2025-09-23 22:09:01 -0300

    add - adicionando tela de lista das motos

--- src/main/java/br/com/fiap/iottu/moto/MotoController.java
+++ src/main/java/br/com/fiap/iottu/moto/MotoController.java
@@ -4,17 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/motos")
+@RequestMapping("/")
 public class MotoController {
 
     @Autowired
     private MotoService motoService;
+
+    @GetMapping
+    public String listarMotos(Model model) {
+        model.addAttribute("motos", motoService.buscarTodas());
+        return "moto/lista";
+    }
 
     @GetMapping("/cadastrar")
     public String mostrarFormularioCadastro(Model model) {
@@ -22,16 +27,10 @@ public class MotoController {
         return "moto/cadastrar";
     }
 
-    @PostMapping
-    public String salvarMoto(@ModelAttribute Moto moto, RedirectAttributes attributes) {
+    @PostMapping("/cadastrar")
+    public String salvarMoto(Moto moto, RedirectAttributes attributes) {
         motoService.salvarMoto(moto);
         attributes.addFlashAttribute("mensagem", "Moto cadastrada com sucesso!");
-        return "redirect:/motos";
-    }
-
-    @GetMapping
-    public String listarMotos(Model model) {
-        model.addAttribute("motos", motoService.buscarTodas());
-        return "moto/lista";
+        return "redirect:/";
     }
 }
